dba608885fa781ddf1f44dfc9822f516f5f5edf7
DanTraTra <dantra2012@gmail.com>
2024-09-23 09:26:03 +1200

    tutorial complete text button fix

--- src/components/GameMenu.tsx
+++ src/components/GameMenu.tsx
@@ -94,7 +94,7 @@ const GameMenu: React.FC<GameMenuProps> = ({changeScreenTo, setTutorialState}) =
                             >
                                 <button
                                     onClick={() => handleModeClick(isSelected, item.label, index)}
-                                    className={`btn btn-sm font-tech items-center justify-center text-lg border-0 px-8 flex-shrink-0 transition-all ${
+                                    className={`btn btn-sm font-tech flex items-center justify-center text-lg border-0 px-8 flex-shrink-0 transition-all ${
                                         isSelected ? 'text-lg' : 'text-sm'
                                     }`}
                                 >
